8be9032acb2c5c0a675738dc6ecf44c96b1c926f
00JCIV00 <jake.crawfordiv@gmail.com>
2022-02-08 15:50:01 -0500

    Minor updates.

--- sceptre_installer.sh
+++ sceptre_installer.sh
@@ -2,8 +2,16 @@
 
 # Author:	Jake Crawford
 # Created:	07 Feb 2022
-# Updated:	07 Feb 2022
-# Details:	Streamlines Sceptre installation
+# Updated:	08 Feb 2022
+# Details:	Streamlines Sceptre installation based on the installation README
+
+
+# Variables
+file=$1
+target_dir="/opt"
+sym_name="sceptre"
+sym_full="$target_dir/$sym_name"
+sym_target=""
 
 # Pre-checks
 # - Check for Root access
@@ -14,18 +22,11 @@ fi
 
 # - Validate file input
 if (($# == 0)); then
-	echo "File to be installed must be specified."
-	exit 1
+	read -p "The file to be installed must be specified. Please enter the file now: " $file
 fi
 
-# Variables
-target_dir="/opt"
-sym_name="sceptre"
-sym_full="$target_dir/$sym_name"
-sym_target=""
-
 # Installation
-echo "Beginnning installation of '$1'..."
+echo "Beginnning installation of ($file)..."
 # - Sym Link
 if test -f "$sym_full"; then
 	echo "Old sym link found. Removing..."
@@ -34,20 +35,28 @@ if test -f "$sym_full"; then
 	echo "Removed sym old link."
 fi
 
+echo "- Identifying tar file extraction method..."
 target_ext="xf"
-if ("$1" == "*.gz"); then
+if [ "$file" == "*.gz" ]; then
 	$target_ext="xzf"
 fi
+echo "- Identified tar file extraction method ($target_ext)."
 
-if extract_file=$(tar -$target_ext $1 -C $target_dir); then
-	$sym_target = $(echo $extract_file | cut -f1 -d" ")
-fi
+echo "- Extracting tar file..."
+# if extract_file=`tar -$target_ext $file -C $target_dir`; then
+# 	$sym_target=`echo $extract_file | head -1 | cut -f1 -d"/"`
+# else
+	echo "- Extraction of tar file failed."
+# 	exit 1
+# fi
+extract_file=`tar -$target_ext $file -C $target_dir` #&& $sym_target=`$extract_file | head -1 | cut -f1 -d"/"`
+echo "- Extracted tar file ($extract_file) to ($sym_target)."
 
-echo "Creating sym link at '$sym_full'..."
+echo "- Creating sym link at '$sym_full'..."
 ln -s $sym_target $sym_full
-echo "Created sym link."
+echo "- Created sym link."
 
 
-echo "Installation of '$1' complete."
+echo "Installation of '$file' complete!"
 exit 0
 
